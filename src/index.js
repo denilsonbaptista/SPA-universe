bdacef16d7d500ef219ff7019a3fa4113779d6f6
Denilson Baptista <baptistadenilsonbp@gmail.com>
2023-09-06 11:26:29 -0300

    changing background when changing pages

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import * as el from "./elements.js"
 import { Router } from "./router.js"
 
 const router = new Router()
-router.add("/", "/pages/home.html")
-router.add("/universe", "/pages//universe.html")
-router.add("/exploration", "/pages/exploration.html")
+router.add("/", 
+  "/pages/home.html", 
+  "url(./assets/mountains-universe-1.png)")
+router.add(
+  "/universe",
+  "/pages//universe.html",
+  "url(./assets/mountains-universe-2.png)"
+)
+router.add(
+  "/exploration",
+  "/pages/exploration.html",
+  "url(./assets/mountains-universe-3.png)"
+)
 
 router.handle()
 
--- src/router.js
+++ src/router.js
@@ -2,9 +2,11 @@ import * as el from "./elements.js"
 
 export class Router {
   routes = {}
+  pageImages = {}
 
-  add(routeName, page) {
+  add(routeName, page, pageBg) {
     this.routes[routeName] = page
+    this.pageImages[routeName] = pageBg
   }
 
   route(event) {
@@ -18,12 +20,13 @@ export class Router {
 
   handle() {
     const { pathname } = window.location
-    const route = this.routes[pathname] ?? this.route["/"]
+    const route = this.routes[pathname]
 
     fetch(route)
       .then((data) => data.text())
       .then((html) => {
         el.app.innerHTML = html
+        el.body.style.backgroundImage = this.pageImages[pathname]
       })
   }
 }
